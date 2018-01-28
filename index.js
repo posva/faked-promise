b2677a21bca9444763b988027671442bdeba8897
Eduardo San Martin Morote <posva13@gmail.com>
2018-01-28 18:11:06 +0100

    🚨 Add linting

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = function fakePromise () {
+  // eslint-disable-next-line
   return new Promise(outerResolve => {
     const promise = new Promise((resolve, reject) => {
       setTimeout(() => {
