0f32691da1162a6c5d00bfe3f47de68a3c3814b3
Eduardo San Martin Morote <posva13@gmail.com>
2018-01-28 21:51:53 +0100

    💡 Add note about async

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ module.exports = function fakePromise () {
   // eslint-disable-next-line
   return new Promise(outerResolve => {
     const promise = new Promise((resolve, reject) => {
+      // if we don't use setTimeout or something similar
+      // we don't have access to promise
       setTimeout(() => {
         outerResolve([promise, resolve, reject])
       }, 0)
