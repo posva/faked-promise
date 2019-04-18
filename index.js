efb4b92105ea38ec33dd831462e0625c9ba84fec
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-18 18:50:14 +0200

    feat: add typing support

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a Promise that can be resolved manually
+ *
+ * @returns {[Promise<any>, (value?: any) => void, (error?: any) => void]}
+ */
 module.exports = function fakePromise () {
   let res, rej
   // the executor is guaranteed to get executed right away
